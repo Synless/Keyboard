5fa619dacab2671dace2403214605da94efcc47d
Cyril <Synless@users.noreply.github.com>
2017-07-07 22:47:40 +0200

    Update keyboard.ino

--- keyboard.ino
+++ keyboard.ino
@@ -33,6 +33,15 @@ uint8_t keyMod[]
           KEY_LEFT_CTRL, KEY_LEFT_ALT, ' ', KEY_RIGHT_ALT, KEY_RIGHT_CTRL,  KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW, KEY_RIGHT_ARROW */
 };
 
+bool key[numberOfKeys] =
+ {
+   true, true,true,true,true,true,true,
+   true, true,true,true,true,true,true,
+   true, true,true,true,true,true,true,
+       true,  true,true,true,  true,
+            true,  true,  true
+ };
+
 int max_x = 512;
 int max_y = 512;
 int min_x = 512;
@@ -79,13 +88,26 @@ void setup()
         //FIRST TIME PLUGING THE KEYBOARD
         storeKeyMod();
     }
+    
 }
-
+long tmp1 = 0;
+long tmp2 = 0;
 void loop()
 {
+    tmp1 = micros();
     serial();
+    tmp2 = micros();
+    //SerialUSB.print("dif serial\t: "); SerialUSB.print((tmp2-tmp1)); SerialUSB.println("us"); SerialUSB.println();
+    tmp1 = micros();
     keypress();
+    tmp2 = micros();
+    //SerialUSB.print("dif keypress\t: "); SerialUSB.print((tmp2-tmp1)); SerialUSB.println("us"); SerialUSB.println();
+    tmp1 = micros();
     joystick();
+    tmp2 = micros();
+    //SerialUSB.print("dif joystick\t: "); SerialUSB.print((tmp2-tmp1)); SerialUSB.println("us"); SerialUSB.println();
+    
+    //delay(200); 
 }
 
 
@@ -190,50 +212,105 @@ void keypress()
     digitalWrite(46, LOW);
     for (int n = 0; n < numberOfKeys; n++)
     { 
-        if (digitalRead(pinMod[n]) == LOW) 
+        if (digitalRead(pinMod[n]) == LOW && key[n]) 
         { 
+            key[n] = false; 
             Keyboard.press(keyMod[n]); 
             digitalWrite(46, HIGH); 
         }
-        else if (digitalRead(pinMod[n]) == HIGH ) 
+        else if (digitalRead(pinMod[n]) == HIGH && !key[n]) 
         { 
+            key[n] = true; 
             Keyboard.release(keyMod[n]); 
         }
     }
 }
+
+//http://forum.arduino.cc/index.php?topic=443173.0
+#if defined(__arm__) 
+int inline analogReadFast(byte ADCpin, byte prescalerBits) // inline library functions must be in header
+{ 
+    ADC->CTRLA.bit.ENABLE = 0;                     // Disable ADC
+    while( ADC->STATUS.bit.SYNCBUSY == 1 );        // Wait for synchronization
+    ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 |   // Divide Clock by 64
+                     ADC_CTRLB_RESSEL_10BIT; 
+    ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 |   // 1 sample 
+                       ADC_AVGCTRL_ADJRES(0x00ul); // Adjusting result by 0
+    ADC->SAMPCTRL.reg = 0x00;                      // Sampling Time Length = 0
+  
+    ADC->CTRLA.bit.ENABLE = 1;                     // Enable ADC
+    while( ADC->STATUS.bit.SYNCBUSY == 1 );        // Wait for synchronization
+    return analogRead(ADCpin);
+}
+#else
+int inline analogReadFast(byte ADCpin, byte prescalerBits) // inline library functions must be in header
+{ 
+    byte ADCSRAoriginal = ADCSRA; 
+    ADCSRA = (ADCSRA & B11111000) | prescalerBits; 
+    int adc = analogRead(ADCpin);  
+    ADCSRA = ADCSRAoriginal;
+    return adc;
+}
+#endif
+
+
+bool releasedX = false;
+bool releasedY = false;
 void joystick()
 {
     //JOYSTICK //KEY_UP_ARROW,KEY_RIGHT_ARROW, KEY_DOWN_ARROW,KEY_LEFT_ARROW
-    current_x = analogRead(A0);
-    current_y = 1023-analogRead(A1);      
-    //SerialUSB.print("A0 : "); SerialUSB.println(current_x);
-    //SerialUSB.print("A1 : "); SerialUSB.println(current_y);
+
+    current_x = analogReadFast(A0,0);
+    current_y = 1023-analogReadFast(A1,0);      
+    /*SerialUSB.print("A0 : "); SerialUSB.println(current_x);
+    SerialUSB.print("A1 : "); SerialUSB.println(current_y);
+    SerialUSB.print("releasedX : "); SerialUSB.println(releasedX);
+    SerialUSB.print("releasedY : "); SerialUSB.println(releasedY);*/
+    //Serial.println();
     //_joystick.setXAxis(current_x);
     //_joystick.setYAxis(current_y);
     //X
     if (x - current_x > threshold)
-    {   Keyboard.press(keyMod[29]);
+    {   
+        releasedX = false;
+        Keyboard.press(keyMod[29]);
         Keyboard.release(keyMod[31]);        
     }
     else if (current_x - x > threshold)
-    {   Keyboard.press(keyMod[31]);
+    {   
+        releasedX = false;
+        Keyboard.press(keyMod[31]);
         Keyboard.release(keyMod[29]);
     }    
     else
-    {   Keyboard.release(keyMod[29]);
-        Keyboard.release(keyMod[31]);
+    {   
+        if(!releasedX)
+        {
+            releasedX = true;
+            Keyboard.release(keyMod[29]);
+            Keyboard.release(keyMod[31]);
+        }
     }
     //Y
     if (y - current_y > threshold)
-    {   Keyboard.press(keyMod[30]);
+    {   
+        releasedY = false;
+        Keyboard.press(keyMod[30]);
         Keyboard.release(keyMod[32]);
     }    
     else if (current_y - y > threshold)
-    {   Keyboard.press(keyMod[32]);
+    {   
+        releasedY = false;
+        Keyboard.press(keyMod[32]);
         Keyboard.release(keyMod[30]);
     }
     else
-    {   Keyboard.release(keyMod[30]);
-        Keyboard.release(keyMod[32]);
+    {   
+        if(!releasedY)
+        {
+            releasedY = true;
+            Keyboard.release(keyMod[30]);
+            Keyboard.release(keyMod[32]);
+        }
     }
 }

c3a0955a7bf5b7093c9b85bf46a8b0a26d76b0dd
Synless <Synless@users.noreply.github.com>
2021-11-15 09:22:51 +0100

    Added useLockingKey
    Added useLockingKey to choose between layouts using a Cherry MX lock and a regular switch

--- QMK part/qmk_firmware/keyboards/synless/keymaps/default/keymap.c
+++ QMK part/qmk_firmware/keyboards/synless/keymaps/default/keymap.c
@@ -1,11 +1,45 @@
 #include QMK_KEYBOARD_H
+#include "keyboard.h"
+#include "analog.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_default(
 		KC_ESC , KC_1 , KC_2 , KC_3 , KC_4, KC_5, KC_6,
 		KC_TAB , KC_Q , KC_W , KC_E , KC_R, KC_T, KC_Y,
 		KC_TAB , KC_A , KC_S , KC_D , KC_F, KC_G, KC_H,
-		KC_RSFT , KC_LSFT , KC_Z , KC_X , KC_C, KC_V, KC_RSFT,
+		_______ , KC_LSFT , KC_Z , KC_X , KC_C, KC_V, KC_RSFT,
 		_______ , KC_LCTL , KC_LWIN , KC_LOPT , KC_SPC, KC_RCTL, _______
     )
 };
+
+//refer to keyboard.c
+bool useLockingKey = true;
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+	if(keycode==KC_6)
+	{
+		if(useLockingKey)
+		{
+			//made to work with a Cherry MX lock on the top line1/column7
+			if(record->event.pressed)
+			{
+				useJoystick = false;
+			}
+			else if(!record->event.pressed)
+			{
+				useJoystick = true;
+			}
+		}
+		else
+		{
+			//made to work with regular key on the top line1/column7, toggle style
+			if(record->event.pressed)
+			{
+				useJoystick = !useJoystick;
+			}
+		}
+
+		return false;
+	}
+	return true;
+}

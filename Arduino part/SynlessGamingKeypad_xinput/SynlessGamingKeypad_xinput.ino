f9e6f1165ffcada6236b4ed035474917b4408b02
Synless <Synless@users.noreply.github.com>
2022-03-01 21:06:27 +0100

    New hardware revision from 20/08/2021

--- Arduino part/SynlessGamingKeypad_xinput/SynlessGamingKeypad_xinput.ino
+++ Arduino part/SynlessGamingKeypad_xinput/SynlessGamingKeypad_xinput.ino
@@ -1,67 +1,84 @@
-#include <XInput.h> //https://github.com/dmadison/ArduinoXInput
+#include <XInput.h>
+#define RUMBLEOUTPUT 17
 
 long x, y, x_start, y_start;
-
-#define RUMBLEOUTPUT 14
+bool rightSideLayout = false;
 
 void setup()
 {
-    pinMode(A1,INPUT);//X
-    pinMode(A0,INPUT);//Y
-    pinMode(A3,INPUT);//Chip select
-    
     pinMode(RUMBLEOUTPUT,OUTPUT);
     digitalWrite(RUMBLEOUTPUT,HIGH);
-  
-    x_start = analogRead(A0);
-    y_start = analogRead(A1);
+
+    pinMode(A1,INPUT); //X or Y
+    pinMode(A0,INPUT); //X or Y
+    pinMode(A3,INPUT); //Chip select - Active LOW
+    pinMode(A4,INPUT); //Right of left layout - LOW = joystick on the right, HIGH = joystick on the left
 
     XInput.setReceiveCallback(rumbleCallback);
     XInput.setAutoSend(true);
     XInput.begin();
-    
-    //Serial.begin(115200);
+}
+
+void rumbleCallback(uint8_t packetType)
+{
+    // If we have an LED packet (0x01), do nothing
+    if (packetType == (uint8_t) XInputReceiveType::LEDs) { return; }
+  6666
+    // If we have a rumble packet (0x00), see our rumble data on the LED
+    else if (packetType == (uint8_t) XInputReceiveType::Rumble)
+    {
+        uint8_t rumbleValue = XInput.getRumbleLeft() | XInput.getRumbleRight();        
+        digitalWrite(RUMBLEOUTPUT,LOW);
+        analogWrite(RUMBLEOUTPUT, rumbleValue);
+    }
 }
 
 const int JoyMax = 32767;
+const int deadZone = 1000;
 int innerBorder_x = 260;
 int innerBorder_y = 260;
 bool once = true;
 
 void loop()
 {
-    //Serial.println(analogRead(A1));
-    
-    if(analogRead(A3) < 1000)
+    if(analogRead(A3) < 1000) //digitalRead does not work for some reason
     {
-        once = true;
+        if(!once) //if this is the first activated cycle ...
+        {
+            rightSideLayout = analogRead(A4) < 1000;
+            
+            if(rightSideLayout) { y_start = analogRead(A0); x_start = analogRead(A1); }
+            else { y_start = analogRead(A1); x_start = analogRead(A0); }
 
-        x = y_start - analogRead(A1);
-        y = x_start - analogRead(A0);        
+            digitalWrite(RUMBLEOUTPUT,LOW);  delay(50); //rumble pattern for activation
+            digitalWrite(RUMBLEOUTPUT,HIGH); delay(90);
+            digitalWrite(RUMBLEOUTPUT,LOW);  delay(50);
+            digitalWrite(RUMBLEOUTPUT,HIGH);
+        }
+        once = true;
         
-        if(abs(x) > innerBorder_x) { innerBorder_x = abs(x); }
-        if(abs(y) > innerBorder_y) { innerBorder_y = abs(y); }   
+        if(rightSideLayout) { y = y_start - analogRead(A0); x = x_start - analogRead(A1); } //getting analog values for x and y
+        else                { y = y_start - analogRead(A1); x = analogRead(A0) - x_start; }
+        
+        if(abs(x) > innerBorder_x) { innerBorder_x = abs(x); } //following the maximum value
+        if(abs(y) > innerBorder_y) { innerBorder_y = abs(y); }
              
-        x = map(x,-innerBorder_x,innerBorder_x,-JoyMax,JoyMax)*1.08;
-        y = map(y,-innerBorder_y,innerBorder_y,-JoyMax,JoyMax)*1.08;
+        x = map(x,-innerBorder_x,innerBorder_x,-JoyMax,JoyMax) * 1.2;
+        y = map(y,-innerBorder_y,innerBorder_y,-JoyMax,JoyMax) * 1.2;
+        x = max(-JoyMax,min(JoyMax,x)); //cropping maximum value to prevent 16 bits integer overflow
+        y = max(-JoyMax,min(JoyMax,y));        
         
-        x = max(-JoyMax,min(JoyMax,x));
-        y = max(-JoyMax,min(JoyMax,y));
+        if(x > -deadZone && x < deadZone){x = 0;} //if the value is too small, replace by 0
+        if(y > -deadZone && y < deadZone){y = 0;}
         
-        XInput.setJoystick(JOY_LEFT, (int)x,(int)y);
+       XInput.setJoystick(JOY_LEFT, (int)x,(int)y);
     }
     else if(once)
     {
+        digitalWrite(RUMBLEOUTPUT,LOW); delay(80);  //rumble pattern for deactivation
+        digitalWrite(RUMBLEOUTPUT,HIGH);
+        
         once = false;
         XInput.setJoystick(JOY_LEFT, 0,0);
-    }
- }
-
-void rumbleCallback(uint8_t packetType)
-{
-    if (packetType == (uint8_t) XInputReceiveType::Rumble)
-    {
-        uint8_t rumbleValue = XInput.getRumbleLeft() | XInput.getRumbleRight();
-        analogWrite(RUMBLEOUTPUT, rumbleValue);
     }
 }

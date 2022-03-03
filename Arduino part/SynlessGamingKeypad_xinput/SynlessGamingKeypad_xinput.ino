3c96663d69266e517f11bbfa58b89e8e2df73360
Synless <Synless@users.noreply.github.com>
2022-03-03 11:05:29 +0100

    async rumble when changing mode

--- Arduino part/SynlessGamingKeypad_xinput/SynlessGamingKeypad_xinput.ino
+++ Arduino part/SynlessGamingKeypad_xinput/SynlessGamingKeypad_xinput.ino
@@ -42,33 +42,53 @@ const int deadZone = 1000;
 int innerBorder_x = 260;
 int innerBorder_y = 260;
 bool once = true;
+int step = 0;
+long t1 = 0;
+long t2 = 0;
 
 void loop()
 {
+    t1 = millis();
+
     if(analogRead(A3) < 1000) //digitalRead does not work for some reason
     {
         if(!once) //if this is the first activated cycle ...
         {
             rightSideLayout = analogRead(A4) < 1000;
             
-            if(rightSideLayout) { y_start = analogRead(A0); x_start = analogRead(A1); }
-            else { y_start = analogRead(A1); x_start = analogRead(A0); }
+            if(rightSideLayout)
+            {
+                y_start = analogRead(A0);
+                x_start = analogRead(A1);
+            }
+            else
+            {
+                y_start = analogRead(A1);
+                x_start = analogRead(A0);
+            }
 
-            digitalWrite(RUMBLEOUTPUT,LOW);  delay(50); //rumble pattern for activation
-            digitalWrite(RUMBLEOUTPUT,HIGH); delay(90);
-            digitalWrite(RUMBLEOUTPUT,LOW);  delay(50);
-            digitalWrite(RUMBLEOUTPUT,HIGH);
+            step = 1;
+            t2 = millis();
         }
+
         once = true;
         
-        if(rightSideLayout) { y = y_start - analogRead(A0); x = x_start - analogRead(A1); } //getting analog values for x and y
-        else                { y = y_start - analogRead(A1); x = analogRead(A0) - x_start; }
+        if(rightSideLayout)
+        {
+            y = y_start - analogRead(A0);
+            x = x_start - analogRead(A1);
+        }
+        else
+        {
+            y = y_start - analogRead(A1);
+            x = analogRead(A0) - x_start;
+        }
         
         if(abs(x) > innerBorder_x) { innerBorder_x = abs(x); } //following the maximum value
         if(abs(y) > innerBorder_y) { innerBorder_y = abs(y); }
              
-        x = map(x,-innerBorder_x,innerBorder_x,-JoyMax,JoyMax) * 1.3;
-        y = map(y,-innerBorder_y,innerBorder_y,-JoyMax,JoyMax) * 1.3;
+        x = map(x,-innerBorder_x,innerBorder_x,-JoyMax,JoyMax) * 1.25;
+        y = map(y,-innerBorder_y,innerBorder_y,-JoyMax,JoyMax) * 1.25;
         x = max(-JoyMax,min(JoyMax,x)); //cropping maximum value to prevent 16 bits integer overflow
         y = max(-JoyMax,min(JoyMax,y));        
         
@@ -79,10 +99,63 @@ void loop()
     }
     else if(once)
     {
-        digitalWrite(RUMBLEOUTPUT,LOW); delay(80);  //rumble pattern for deactivation
-        digitalWrite(RUMBLEOUTPUT,HIGH);
-        
-        once = false;
+        once = false;        
+        step = -1;
+        t2 = millis();
         XInput.setJoystick(JOY_LEFT, 0,0);
+    }
+
+    //async rumble when changing mode
+    if(step == 1)
+    {
+        digitalWrite(RUMBLEOUTPUT,LOW);
+        
+        if((t1-t2) > 50)
+        {
+            t2 = t1;
+            step = 2;
+        }
+    }
+    else if(step == 2)
+    {
+        digitalWrite(RUMBLEOUTPUT,HIGH);
+
+        if((t1-t2) > 90)
+        {
+            t2 = t1;
+            step = 3;
+        }
+    }
+    else if(step == 3)
+    {
+        digitalWrite(RUMBLEOUTPUT,LOW);
+
+        if((t1-t2) > 50)
+        {
+            t2 = t1;
+            step = 4;
+        }
+    }
+    else if(step == 4)
+    {
+        digitalWrite(RUMBLEOUTPUT,HIGH);
+        t2 = t1;
+        step = 0;
+    }
+    else if(step == -1)
+    {
+        digitalWrite(RUMBLEOUTPUT,LOW);       
+         
+        if((t1-t2) > 80)
+        {
+            t2 = t1;
+            step = -2;
+        }
+    }
+    else if(step == -2)
+    {
+        digitalWrite(RUMBLEOUTPUT,HIGH);
+        t2 = t1;
+        step = 0;
     }
 }
